987e67512cbceb9f1c4969509525643c850dd865
kojilbj <20020427ka@gmail.com>
2025-11-11 00:36:03 +0100

    format: flake8

--- ex01/S1E7.py
+++ ex01/S1E7.py
@@ -4,38 +4,47 @@ from S1E9 import Character
 class Baratheon(Character):
     """Representing the Baratheon family."""
     def __init__(self, first_name: str, is_alive: bool = True):
+        """Constructor"""
         super().__init__(first_name, is_alive)
         self.family_name = "Baratheon"
         self.eyes = "brown"
         self.hairs = "dark"
 
     def die(self):
+        """This function change is_alive into false"""
         self.is_alive = False
 
     def __str__(self):
+        """String"""
         return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
     def __repr__(self):
+        """Representing"""
         return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
 
 class Lannister(Character):
     """Representing the Lannister family."""
     def __init__(self, first_name: str, is_alive: bool = True):
+        """Constructor"""
         super().__init__(first_name, is_alive)
         self.family_name = "Lannister"
         self.eyes = "blue"
         self.hairs = "light"
 
     def die(self):
+        """This function change is_alive into false"""
         self.is_alive = False
 
     def __str__(self):
+        """String"""
         return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
     def __repr__(self):
+        """Representing"""
         return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
     def create_lannister(first_name: str, is_alive: bool):
+        """This function return new lannister instance."""
         lannister = Lannister(first_name, is_alive)
         return lannister
