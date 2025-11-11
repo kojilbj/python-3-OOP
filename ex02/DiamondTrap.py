6b9c6585219950bdfdb0865dcf6e995499efc00e
kojilbj <20020427ka@gmail.com>
2025-11-11 01:03:35 +0100

    feature: ex02 done

--- ex02/DiamondTrap.py
+++ ex02/DiamondTrap.py
@@ -1,5 +1,24 @@
 from S1E7 import Baratheon, Lannister
 
+
 class King(Baratheon, Lannister):
-    def __init__():
-        print("hello")
+    """False King"""
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """Constructor"""
+        super().__init__(first_name, is_alive)
+
+    def set_hairs(self, hairs: str):
+        """This function set hairs attribute."""
+        self.hairs = hairs
+
+    def set_eyes(self, eyes: str):
+        """This function set eyes attribute."""
+        self.eyes = eyes
+
+    def get_hairs(self):
+        """This function return hairs attribute."""
+        return self.hairs
+
+    def get_eyes(self):
+        """This function return eyes attribute."""
+        return self.eyes
